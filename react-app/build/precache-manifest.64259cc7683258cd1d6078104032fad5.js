2650475651d670582ec4d884e3a9c198a7d4276b
dainq97 <nqdai2904@gmail.com>
2022-04-21 09:37:23 +0700

    update build

---
+++ react-app/build/precache-manifest.64259cc7683258cd1d6078104032fad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b91c8fa590578cebf20a0518bbf9b1",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/index.html"
+  },
+  {
+    "revision": "3cde183904d8f31c380b",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "8f00c1fd6e10af27143f",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3cde183904d8f31c380b",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/js/2.fd0622b9.chunk.js"
+  },
+  {
+    "revision": "564a8286d91b6de9c0fef56cd10b7ba3",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/js/2.fd0622b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f00c1fd6e10af27143f",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/js/main.cb9d495b.chunk.js"
+  },
+  {
+    "revision": "d9e40c591969c4d949ce",
+    "url": "/wp-content/plugins/wp-create-react-app-concept/react-app/build/static/js/runtime-main.3ffc7586.js"
+  }
+]);
